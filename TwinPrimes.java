9b2fbc3a3e2a4335f339a2ca5aea905eee8ed354
Jenny <31353637+jenchen08@users.noreply.github.com>
2019-02-08 23:17:59 -0500

    Add files via upload
    Twin Primes... can translate to Javascript

---
+++ TwinPrimes.java
@@ -0,0 +1,50 @@
+// Java program to print all Twin Prime  
+// Numbers using Sieve of Eratosthenes 
+import java.io.*; 
+  
+class TwinPrimes { 
+  
+    static void printTwinPrime(int n) 
+    { 
+        // Create a boolean array "prime[0..n]"  
+        // and initialize all entries it as  
+        // true. A value in prime[i] will 
+        // finally be false if i is Not a  
+        // prime, else true. 
+        boolean prime[] = new boolean[n + 1]; 
+          
+        for (int i = 0; i <= n; i++) 
+            prime[i] = true; 
+  
+        for (int p = 2; p * p <= n; p++) { 
+              
+            // If prime[p] is not changed, 
+            // then it is a prime 
+            if (prime[p] == true) { 
+                  
+                // Update all multiples of p 
+                for (int i = p * 2; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+  
+        // to check for twin prime numbers 
+        // display th twin prime 
+        for (int i = 2; i <= n - 2; i++) { 
+  
+            if (prime[i] == true && 
+                prime[i + 2] == true) 
+              
+                // Display the result 
+                System.out.print(" (" + i + ", " + 
+                                   (i + 2) + ")"); 
+        } 
+    } 
+  
+    // Driver Program to test above function 
+    public static void main(String args[]) 
+    { 
+        int n = 1000; 
+        printTwinPrime(n); 
+    } 
+} 
